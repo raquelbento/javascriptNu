61ae5ab71212ed36e628c759e7e622c51fbfbdff
raquelbento <123309696+raquelbento@users.noreply.github.com>
2023-05-23 10:46:04 -0300

    testando o js

--- aulajavascript/main.js
+++ aulajavascript/main.js
@@ -3,3 +3,12 @@ var a = 10;
 var b = 20;
 var total = a + b;
 console.log(total);
+
+var nomePessoa = "Lucas";
+console.log(nomePessoa);
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+    console.log(var3)
+
